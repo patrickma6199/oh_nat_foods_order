9c66ce446e287296eac1c238eadf3e63acadb22a
Patrick Ma <patrickma6199@gmail.com>
2023-11-30 10:32:36 -0800

    finished initial home page and mapped buttons

--- oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/MainActivity.java
+++ oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.oh_nat_foods_order;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLogin() {
+        Intent toLogin = new Intent(this,Login.class);
+        startActivity(toLogin);
+    }
+
+    public void onRegister() {
+        Intent toRegister = new Intent(this, register.class);
+    }
+
+    public void onHome() {
+        Toast.makeText(this, "You are already in the Home Page!", Toast.LENGTH_SHORT).show();
     }
 }
