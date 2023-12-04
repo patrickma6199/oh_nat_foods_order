e17f12de2e9f47ddc7a4b5413dfe1da473540e7e
DanKelowna <dankelowna@gmail.com>
2023-12-03 20:45:26 -0800

    Added login/register UI and basic interaction

--- oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/Login.java
+++ oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/Login.java
@@ -2,13 +2,57 @@ package com.example.oh_nat_foods_order;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Login extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private TextView registerPromptText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize UI elements
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+        registerPromptText = findViewById(R.id.registerPromptText);
+
+        //Login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                loginUser(email, password);
+            }
+        });
+
+        //Register button
+        registerPromptText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        //Implement login backend
     }
 }
--- oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/register.java
+++ oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/register.java
@@ -2,13 +2,66 @@ package com.example.oh_nat_foods_order;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class register extends AppCompatActivity {
+
+    private ImageView logoImageView;
+    private TextView titleTextView;
+    private EditText nameEditText;
+    private EditText registerEmailEditText;
+    private EditText registerPasswordEditText;
+    private Button registerButton;
+    private TextView alreadyHaveAccountText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Initialize UI elements
+        logoImageView = findViewById(R.id.logoImageView);
+        titleTextView = findViewById(R.id.titleTextView);
+        nameEditText = findViewById(R.id.nameEditText);
+        registerEmailEditText = findViewById(R.id.registerEmailEditText);
+        registerPasswordEditText = findViewById(R.id.registerPasswordEditText);
+        registerButton = findViewById(R.id.registerButton);
+        alreadyHaveAccountText = findViewById(R.id.alreadyHaveAccountText);
+
+        //Register button
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code to perform on register button click
+                String name = nameEditText.getText().toString();
+                String email = registerEmailEditText.getText().toString();
+                String password = registerPasswordEditText.getText().toString();
+                registerUser(name, email, password);
+            }
+        });
+        //Already have an account button
+        alreadyHaveAccountText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(register.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void registerUser(String name, String email, String password) {
+        // Implement backend here
     }
 }
