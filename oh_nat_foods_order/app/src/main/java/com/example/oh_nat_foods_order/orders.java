ee122a089fa673442ce6e1fbbc20ec050578bbea
Patrick Ma <patrickma6199@gmail.com>
2023-12-05 08:15:16 -0800

    mapped button from orders to accountsummary

--- oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/orders.java
+++ oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/orders.java
@@ -2,7 +2,10 @@ package com.example.oh_nat_foods_order;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class orders extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class orders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+    }
+
+    public void onLogin(View view) {
+        //should be logout button, we can implement dynamically
+    }
+
+    public void onHome(View view) {
+        Toast.makeText(this,"You're already in the home orders page!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onAccount(View view) {
+        Intent toSummary = new Intent(this,accountsummary.class);
+        startActivity(toSummary);
+        finish();
     }
 }
