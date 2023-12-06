1bf2dd197fa053f53ae9f281dce14d7aaf7eb08d
DanKelowna <dankelowna@gmail.com>
2023-12-05 22:54:43 -0800

    Confirm button slightly reduced size

--- oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/paymentMethods.java
+++ oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/paymentMethods.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -47,11 +48,13 @@ public class paymentMethods extends AppCompatActivity {
                     deleteButton.setOnClickListener(v -> {
                         if (lastClickedDeleteButton != null && lastClickedDeleteButton != deleteButton) {
                             lastClickedDeleteButton.setText("Delete");
+                            lastClickedDeleteButton.setTextSize(TypedValue.COMPLEX_UNIT_PX, lastClickedDeleteButton.getTextSize());
                         }
                         if (deleteButton.getText().toString().equals("Confirm?")) {
                             deletePaymentMethod(cardNumber);
                         } else {
                             deleteButton.setText("Confirm?");
+                            deleteButton.setTextSize(TypedValue.COMPLEX_UNIT_PX, deleteButton.getTextSize() - getResources().getDisplayMetrics().scaledDensity);
                             lastClickedDeleteButton = deleteButton;
                         }
                     });
