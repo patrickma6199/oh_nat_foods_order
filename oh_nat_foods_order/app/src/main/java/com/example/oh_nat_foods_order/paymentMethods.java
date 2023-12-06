3577ac84d3da3e5c180cd074cf6e345869760f17
DanKelowna <dankelowna@gmail.com>
2023-12-05 22:04:12 -0800

    Added payment method scroll

--- oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/paymentMethods.java
+++ oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/paymentMethods.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,58 +17,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class paymentMethods extends AppCompatActivity {
 
     private SharedPreferences shared;
     private String uid;
-
     private DatabaseReference methods;
-
     private Button addNewMethod;
+    private LinearLayout paymentMethodsContainer;
 
     protected OnCompleteListener<DataSnapshot> onFetched = new OnCompleteListener<DataSnapshot>() {
         @Override
         public void onComplete(@NonNull Task<DataSnapshot> task) {
-            if(!task.isSuccessful()) {
-                Log.e("firebase","error reading payment methods");
+            if (!task.isSuccessful()) {
+                Log.e("firebase", "error reading payment methods");
             } else {
                 DataSnapshot origin = task.getResult();
-                for(DataSnapshot method: origin.getChildren()) {
-                    cardNumber = method.getKey();
-                    cvv = method.child("CVV").getValue().toString();
-                    postalCode = method.child("postalCode").getValue().toString();
-                    expiryDate = method.child("expiryDate").getValue().toString();
-                    country = method.child("country").getValue().toString();
-                }
+                for (DataSnapshot method : origin.getChildren()) {
+                    // Get payment method details
+                    String cardNumber = method.getKey();
 
+                    // Inflate payment method item view
+                    View paymentMethodView = LayoutInflater.from(paymentMethods.this).inflate(R.layout.payment_method_item, paymentMethodsContainer, false);
+                    TextView paymentMethodText = paymentMethodView.findViewById(R.id.paymentMethodText);
+                    Button deleteButton = paymentMethodView.findViewById(R.id.deletePaymentMethodButton);
+
+                    // Set text and delete button action
+                    paymentMethodText.setText("Card **** **** **** " + cardNumber.substring(cardNumber.length() - 4));
+                    deleteButton.setOnClickListener(v -> deletePaymentMethod(cardNumber));
+
+                    paymentMethodsContainer.addView(paymentMethodView);
+                }
             }
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paymentmethods);
 
-        shared = getApplicationContext().getSharedPreferences("mySession",MODE_PRIVATE);
-        uid = shared.getString("uid","");
+        shared = getApplicationContext().getSharedPreferences("mySession", MODE_PRIVATE);
+        uid = shared.getString("uid", "");
 
         methods = FirebaseDatabase.getInstance().getReference().child("Users").child(uid).child("paymentMethod");
 
-        //can fetch user's payment info here with:
+        paymentMethodsContainer = findViewById(R.id.paymentMethodsContainer);
+
         methods.get().addOnCompleteListener(onFetched);
 
-        addNewMethod = (Button) findViewById(R.id.addPaymentMethodButton);
+        addNewMethod = findViewById(R.id.addPaymentMethodButton);
+        addNewMethod.setOnClickListener(v -> {
+            Intent toAddPayments = new Intent(paymentMethods.this, addPaymentMethod.class);
+            startActivity(toAddPayments);
+            finish();
+        });
+    }
 
-        addNewMethod.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent toAddPayments = new Intent(paymentMethods.this,addPaymentMethod.class);
-                startActivity(toAddPayments);
-                finish();
-            }
+    private void deletePaymentMethod(String cardNumber) {
+        // Delete payment method from Firebase
+        methods.child(cardNumber).removeValue().addOnSuccessListener(aVoid -> {
+            Toast.makeText(paymentMethods.this, "Payment Method Deleted", Toast.LENGTH_SHORT).show();
+            // Refresh the list
+            recreate();
+        }).addOnFailureListener(e -> {
+            Toast.makeText(paymentMethods.this, "Failed to Delete Payment Method", Toast.LENGTH_SHORT).show();
         });
     }
 }
