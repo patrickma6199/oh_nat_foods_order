7776b46652407325d7b925318af9655a5046cc52
patrickma6199 <patrickma6199@gmail.com>
2023-12-06 14:23:16 -0800

    Added menu bar into main and orders

--- oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/myorders.java
+++ oh_nat_foods_order/app/src/main/java/com/example/oh_nat_foods_order/myorders.java
@@ -2,32 +2,20 @@ package com.example.oh_nat_foods_order;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class myorders extends AppCompatActivity {
 
@@ -38,7 +26,6 @@ public class myorders extends AppCompatActivity {
     private String uid;
 
     private LinearLayout myOrdersContainer;
-    TextView homeButton, accountButton, ordersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,92 +35,99 @@ public class myorders extends AppCompatActivity {
         shared = getApplication().getSharedPreferences("mySession",MODE_PRIVATE);
         uid = shared.getString("uid","");
 
-        myOrdersContainer = (LinearLayout) findViewById(R.id.myOrdersContainer);
+        myOrdersContainer = findViewById(R.id.myOrdersContainer);
 
         root = FirebaseDatabase.getInstance().getReference();
         orders = root.child("Orders").child(uid);
+
+        fetchOrders();
+    }
+
+    private void fetchOrders() {
         orders.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 myOrdersContainer.removeAllViews();
                 for(DataSnapshot item: snapshot.getChildren()) {
-                    View myOrderView = LayoutInflater.from(myorders.this).inflate(R.layout.orders_list_item, myOrdersContainer, false);
-
-                    TextView orderOrderId = (TextView) myOrderView.findViewById(R.id.myOrdersNumText);
-                    TextView orderDetails = (TextView) myOrderView.findViewById(R.id.myOrdersDetailsText);
-                    TextView orderQueueNumber = (TextView) myOrderView.findViewById(R.id.myOrdersQueueText);
-                    TextView orderStatus = (TextView) myOrderView.findViewById(R.id.myOrdersStatusText);
-
-                    String orderId = item.getKey();
-                    String queueNumber = item.child("Queue").getValue().toString();
-                    String details = item.child("Items").getValue().toString();
-
-                    orderOrderId.setText(orderId);
-                    orderQueueNumber.setText(queueNumber);
-                    orderDetails.setText(details);
-
-                    if(!queueNumber.equals("0")) {
-
-                        // cancel button
-                        orderStatus.setText("CANCEL");
-                        orderStatus.setOnClickListener(v -> {
-                            cancelOrder(orderId);
-                        });
-
-                    } else {
-
-                        //pick up button
-                        orderStatus.setText("PICK UP");
-                        orderStatus.setOnClickListener(v -> {
-
-                        });
-
-                    }
+                    populateOrderView(item);
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(myorders.this, "Error fetching orders", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        //initializing bottom menu bar
-        //home = (TextView) findViewById(R.id.home);
+    private void populateOrderView(DataSnapshot item) {
+        View myOrderView = LayoutInflater.from(this).inflate(R.layout.orders_list_item, myOrdersContainer, false);
+
+        TextView orderOrderId = myOrderView.findViewById(R.id.myOrdersNumText);
+        TextView orderDetails = myOrderView.findViewById(R.id.myOrdersDetailsText);
+        TextView orderQueueNumber = myOrderView.findViewById(R.id.myOrdersQueueText);
+        TextView orderStatus = myOrderView.findViewById(R.id.myOrdersStatusText);
+
+        String orderId = item.getKey();
+        String queueNumber = item.child("Queue").getValue(String.class);
+        String details = item.child("Items").getValue(String.class);
+
+        orderOrderId.setText(orderId);
+        orderQueueNumber.setText(queueNumber);
+        orderDetails.setText(details);
+
+        setupOrderStatusButton(orderStatus, queueNumber, orderId);
+
+        myOrdersContainer.addView(myOrderView);
+    }
+
+    private void setupOrderStatusButton(TextView orderStatus, String queueNumber, String orderId) {
+        if(!"0".equals(queueNumber)) {
+            orderStatus.setText("CANCEL");
+            orderStatus.setOnClickListener(v -> cancelOrder(orderId));
+        } else {
+            orderStatus.setText("PICK UP");
+            orderStatus.setOnClickListener(v -> {
+                // Implement pick-up logic
+            });
+        }
     }
 
     public void cancelOrder(String orderId) {
         orders.child(orderId).removeValue().addOnSuccessListener(aVoid -> {
+            Toast.makeText(myorders.this, "Order Cancelled", Toast.LENGTH_SHORT).show();
             decrementQueueNumbers(orderId);
+        }).addOnFailureListener(e -> {
+            Toast.makeText(myorders.this, "Failed to Cancel Order", Toast.LENGTH_SHORT).show();
         });
     }
 
     private void decrementQueueNumbers(String orderId) {
         DatabaseReference allOrdersRef = root.child("Orders");
 
-        // Retrieve the current queue numbers
         allOrdersRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // Iterate through each user
-                for(DataSnapshot snapshot: dataSnapshot)
-                if(dataSnapshot.getKey().equals("nextQueue")) {
-                    String currentNum = dataSnapshot.getValue().toString();
-                    String newNum = (Integer.parseInt(currentNum) - 1) + "";
-                    dataSnapshot.getRef().setValue(newNum);
-                } else {
-                    for (DataSnapshot queueSnapshot : dataSnapshot.getChildren()) {
-                        // Iterate through each order
-                        queueSnapshot.
-                    }
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    adjustQueueForUser(userSnapshot);
                 }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                // Handle errors
+                Toast.makeText(myorders.this, "Error updating queue", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    private void adjustQueueForUser(DataSnapshot userSnapshot) {
+        for (DataSnapshot orderSnapshot : userSnapshot.getChildren()) {
+            String queueNumber = orderSnapshot.child("Queue").getValue(String.class);
+            if (queueNumber != null && !queueNumber.equals("0")) {
+                int newQueueNum = Integer.parseInt(queueNumber) - 1;
+                orderSnapshot.getRef().child("Queue").setValue(Integer.toString(newQueueNum));
+            }
+        }
+    }
 }
